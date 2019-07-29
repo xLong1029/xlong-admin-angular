574f82b6eaf598c12e853cfa16cc86cd44f5a054
xLong <381612175@qq.com>
2019-07-29 09:15:20 +0800

    重构

--- _mock/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './_user';
-export * from './_menu';
--- src/style-icons.ts
+++
@@ -1,4 +0,0 @@
-// 静态加载nz-zorro图标
-import { HomeFill, AppstoreFill, ContactsFill } from '@ant-design/icons-angular/icons';
-
-export const ICONS = [HomeFill, AppstoreFill, ContactsFill];
