1151e7ad82e5b74efffec9c9f8813eb4ec1e0680
xLong <381612175@qq.com>
2019-09-12 10:32:18 +0800

    优化：时间格式化方法

--- src/app/shared/utils/timeTrans.ts
+++ src/app/shared/utils/timeTrans.ts
@@ -1,10 +1,10 @@
 /**
- * 根据时间辍转换对应的时间 如 yyyy-MM-dd HH:mm:ss  yyyy-MM-dd
+ * 根据时间辍转换对应的时间 如 YYYY-MM-DD hh:mm:ss  YYYY-MM-DD
  * @param date 时间 辍
- * @param format 时间格式 默认返回年月日时分秒 按照的格式化是 yyyy-MM-dd HH:mm:ss
+ * @param format 时间格式 默认返回年月日时分秒 按照的格式化是 YYYY-MM-DD hh:mm:ss
  * @param timeWord 默认返回2018年01月15日 16:26:30  这种  如填写 - 的。则返回 2018-01-15 16:26:30
  */
-export function timeTrans(date: number, format: string = 'yyyy-MM-dd HH:mm:ss', timeWord: string = '') {
+export function timeTrans(date: number, format: string = 'YYYY-MM-DD hh:mm:ss', timeWord: string = '') {
   date = date.toString().length === 13 ? date : date * 1000;
   const time = new Date(date); /*如果date为13位不需要乘1000 */
   const YType = timeWord === '' ? '年' : timeWord;
@@ -18,28 +18,28 @@ export function timeTrans(date: number, format: string = 'yyyy-MM-dd HH:mm:ss', 
   const m = (time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes()) + ':';
   const s = time.getSeconds() < 10 ? '0' + time.getSeconds() : time.getSeconds();
   switch (format) {
-    case 'yyyy-MM-dd HH:mm:ss':
+    case 'YYYY-MM-DD hh:mm:ss':
       dataValue = Y + M + D + h + m + s;
       break;
-    case 'yyyy':
+    case 'YYYY':
       dataValue = time.getFullYear().toString();
       break;
     case 'MM':
       dataValue = M.substring(0, M.length - 1);
       break;
-    case 'dd':
+    case 'DD':
       dataValue = D.substring(0, D.length - 1);
       break;
-    case 'yyyy-MM':
+    case 'YYYY-MM':
       dataValue = Y + M.substring(0, M.length - 1);
       break;
-    case 'yyyy-MM-dd':
+    case 'YYYY-MM-DD':
       dataValue = Y + M + D;
       break;
-    case 'MM-dd':
+    case 'MM-DD':
       dataValue = M + D.substring(0, D.length - 1);
       break;
-    case 'HH':
+    case 'hh':
       dataValue = h.substring(0, h.length - 1);
       break;
     case 'mm':
@@ -48,10 +48,10 @@ export function timeTrans(date: number, format: string = 'yyyy-MM-dd HH:mm:ss', 
     case 'ss':
       dataValue = s.toString();
       break;
-    case 'HH:mm':
+    case 'hh:mm':
       dataValue = h + m.substring(0, m.length - 1);
       break;
-    case 'HH:mm:ss':
+    case 'hh:mm:ss':
       dataValue = h + m + s;
       break;
     case 'mm:ss':
