58354c9423f8723460309ce47979373069b7f869
xlong1029 <381612175@qq.com>
2020-03-26 18:29:52 +0800

    修改气象接口

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   SERVER_URL: `./`,
-  WEATHER_URL: `https://way.jd.com/he/freeweather`, // 气象接口
+  WEATHER_URL: `/weather`, // 气象接口
   production: true,
   useHash: true,
   hmr: false,
